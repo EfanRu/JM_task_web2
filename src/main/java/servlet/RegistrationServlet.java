5be08b8cf69560194f7479470c5ca03da25e79d9
EfanRu <ffgeniy@mail.ru>
2019-11-18 13:05:33 +0300

    Try write servlets and html pages.

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -1,16 +1,38 @@
 package servlet;
 
+import util.PageGenerator;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
+        Map<String, Object> pageVariables = createPageVariablesMap(req);
+        //For test
+        System.out.println("In RegistrationServlet method GET");
+
+        resp.getWriter().println(PageGenerator.getInstance().getPage("registerPage.html", pageVariables));
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("method", request.getMethod());
+        pageVariables.put("URL", request.getRequestURL().toString());
+        //Need change to login and password
+//        pageVariables.put("Parameter", request.getRequestURL().toString());
+        return pageVariables;
     }
 
     @Override
